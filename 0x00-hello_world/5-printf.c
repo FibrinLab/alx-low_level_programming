96a23f123c52f9d58b7796189b3cbc139fe76a0f
root <fibrinlab@gmail.com>
2022-03-10 06:10:21 -0800

    ii

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * main - with proper grammar, but the outcome is a piece of art,
+ * Return 0 is successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
