a1caaf31c4ef2aeca047cf5b97a09ea65651bcb5
root <fibrinlab@gmail.com>
2022-03-10 06:18:42 -0800

    dd

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /*
- * main - with proper grammar, but the outcome is a piece of art,
+ * main - prints the pharse with proper grammar, 
+ * but the outcome is a piece of art,
  * Return 0 is successful
  */
 
