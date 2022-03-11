25314d214f9256a1a267328bce99b2923c355141
root <fibrinlab@gmail.com>
2022-03-11 11:26:48 -0800

    switched to printf

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,15 +13,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		puts("is positive");
+		printf("is positive");
 	}
 	else if (n == 0)
 	{
-		puts("is zero");
+		printf("is zero");
 	}
 	else
 	{
-		puts("is negative");
+		printf("is negative");
 	}
 	return (0);
 }
