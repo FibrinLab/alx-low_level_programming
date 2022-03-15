eaabcd20dab7335ec58aa96c8d6539cbad185e84
root <fibrinlab@gmail.com>
2022-03-15 16:09:50 -0700

    second

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,10 +3,16 @@
 #include <stdlib.h>
 
 /**
- *
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
  */
 
 int _abs(int)
 {
-	return(abs(int));
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n)
 }
