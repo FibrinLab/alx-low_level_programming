5f2d9060766a742cd01e82c9fefd83fb802f1f55
root <fibrinlab@gmail.com>
2022-03-15 14:52:33 -0700

    first try

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * int _islower(int c - checks for lowercase character
+ */
+
+int _islower(int c)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == c)
+		{
+			return ("1")
+		}
+		else
+		{
+			return ("0")
+		}
+	}
+}
