576d7993fc1461f8c9dfdac16cd271698e982258
root <fibrinlab@gmail.com>
2022-03-15 08:00:05 -0700

    Prints the putf

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar, followed by a new line
+ * Returns (0) if execution is successful 
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
