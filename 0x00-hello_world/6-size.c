2e7c09c82c1b12e87dcc897c6cb3e7327c7e399b
FibrinLab <fibrinlab@gmail.com>
2022-09-13 23:47:44 -0700

    trying sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the size of various types
+ *
+ * Returns: Always (0) success
+ */
+
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeOf(char));
+	printf("Size of an int: %d byte(s)", sizeOf(int));
+	printf("Size of a long int: %d byte(s)", sizeOf(long);
+	return (0);
+}
