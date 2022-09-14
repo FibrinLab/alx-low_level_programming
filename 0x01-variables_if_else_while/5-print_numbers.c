cf464f14a3406a2dca45d51a23e053b7645a8bf4
FibrinLab <fibrinlab@gmail.com>
2022-09-14 13:41:26 -0700

    fifth project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit number
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		putchar(c);
+	}
+	printf("\n");
+	return (0);
+}
