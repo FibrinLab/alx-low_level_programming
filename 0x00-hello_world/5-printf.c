d62de74284dc7ad2dbd0090e60fe07cf359eb93b
FibrinLab <fibrinlab@gmail.com>
2022-09-13 23:28:30 -0700

    used the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a text to the screen followed by a new line
+ *
+ * Returns - Zero if successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
