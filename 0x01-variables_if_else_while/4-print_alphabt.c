df8af61701567421d9d4f8c775e06a4d06fb8cae
FibrinLab <fibrinlab@gmail.com>
2022-09-14 13:18:47 -0700

    added new line

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints some letters in lower case
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+		{
+			putchar('');
+		}
+		else
+		{
+			putchar(c);
+		}
+	}
+	return (0);
+}
