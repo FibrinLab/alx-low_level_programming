e40627d049d05202a13c0ee7afe65a91b2dd6ba0
FibrinLab <fibrinlab@gmail.com>
2022-09-13 23:49:43 -0700

    modified sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-	printf("Size of a char: %d byte(s)", sizeOf(char));
-	printf("Size of an int: %d byte(s)", sizeOf(int));
-	printf("Size of a long int: %d byte(s)", sizeOf(long);
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of an int: %d byte(s)", sizeof(int));
+	printf("Size of a long int: %d byte(s)", sizeof(long);
 	return (0);
 }
