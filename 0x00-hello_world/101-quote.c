58147922869222dcdd9dfa8efdbfe9f090df3d6f
FibrinLab <fibrinlab@gmail.com>
2022-09-14 02:37:20 -0700

    using a new function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stderr);
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
