35e83ab58bfa71d5512fcc58af7cdd2f806706d3
FibrinLab <fibrinlab@gmail.com>
2022-09-14 12:48:31 -0700

    lower case project

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lower case
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		print("%c\n", c);
+	}
+	return (0)
+}
