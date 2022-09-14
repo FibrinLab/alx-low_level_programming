9ee6e9ee057dc3aee775551f7736e3345eebc17f
FibrinLab <fibrinlab@gmail.com>
2022-09-14 02:15:19 -0700

    last one

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints to stderr
+ *
+ * Returns: Always 0 (success)
+ */
+
+int main (void)
+{
+	fprintf(stderr, "and that piece of art is useful\" \n- Dora Korpar, 2015-10-19");
+	return (1);
+}
