6ce7488436673d2f2124c0de9c159deb452bba50
FibrinLab <fibrinlab@gmail.com>
2022-09-16 01:11:21 -0700

    testing the 4th project

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if an alphabetic character
+ *
+ * Return: Always 0 (success)
+ */
+
+int _isalpha(int c)
+{
+	char c;
+
+	for (c = 'A'; c <= 'z'; c++)
+	{
+		_putchar(c)
+	}
+	return (0);
+}
