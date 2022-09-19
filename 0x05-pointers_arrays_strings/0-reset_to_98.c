17f3047ee3f68b6c84f72b717848d41c03f58a99
FibrinLab <fibrinlab@gmail.com>
2022-09-19 01:05:20 -0700

    betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,11 @@
 
 /**
  * reset_to_98 - updates the value using a pointer
- *
+ * @n - interger to a pointer is updated
  * Return: Always 0 (success)
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return (0);
 }
